6298417bea4ff4d886a5c80f3c82e3902baa96ba
k-gn <rlarbska97@gmail.com>
2022-11-22 09:33:25 +0900

    feat: add exception handler test code

---
+++ src/test/java/com/triple/destination_management/global/controller/ApiExceptionHandlerTest.java
@@ -0,0 +1,74 @@
+package com.triple.destination_management.global.controller;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.lang.reflect.Executable;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.handler.DispatcherServletWebRequest;
+
+import com.triple.destination_management.global.constants.ResponseCode;
+import com.triple.destination_management.global.dto.ApiErrorResponse;
+import com.triple.destination_management.global.exception.GeneralException;
+
+@DisplayName("** [ApiExceptionHandlerTest - API 에러 처리] **")
+class ApiExceptionHandlerTest {
+
+	private ApiExceptionHandler exceptionHandler;
+
+	private WebRequest webRequest;
+
+	@BeforeEach
+	void setUp() {
+		exceptionHandler = new ApiExceptionHandler();
+		webRequest = new DispatcherServletWebRequest(new MockHttpServletRequest());
+	}
+
+	@DisplayName("# [1] 프로젝트 일반 오류")
+	@Test
+	void generalException() {
+		// Given
+		ResponseCode responseCode = ResponseCode.INTERNAL_ERROR;
+		GeneralException ex = new GeneralException(responseCode);
+
+		// When
+		ResponseEntity<Object> response = exceptionHandler.general(ex, webRequest);
+
+		// Then
+		assertThat(response)
+			.hasFieldOrPropertyWithValue("body", ApiErrorResponse.of(false, responseCode, ex))
+			.hasFieldOrPropertyWithValue("headers", HttpHeaders.EMPTY)
+			.hasFieldOrPropertyWithValue("statusCode", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@DisplayName("# [2] 기타(전체) 오류")
+	@Test
+	void exception() {
+		// Given
+		Exception ex = new Exception();
+
+		// When
+		ResponseEntity<Object> response = exceptionHandler.exception(ex, webRequest);
+
+		// Then
+		assertThat(response)
+			.hasFieldOrPropertyWithValue("body", ApiErrorResponse.of(false, ResponseCode.INTERNAL_ERROR, ex))
+			.hasFieldOrPropertyWithValue("headers", HttpHeaders.EMPTY)
+			.hasFieldOrPropertyWithValue("statusCode", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
